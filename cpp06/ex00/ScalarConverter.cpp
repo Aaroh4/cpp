4723d39ed75787c8979ac4dcd4bf0b6e22fea1a7
Aaro Hamalainen <aaro.h4malainen@gmail.com>
2024-09-02 15:32:26 +0300

    Progressed in the cpp06

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,5 +1,48 @@
 #include "ScalarConverter.hpp"
 
+int	converter(std::string input, int &i, char &c, float &f, double &d, types type)
+{
+	try
+	{
+		switch (type)
+		{
+			case CHAR:
+				c = input[0];
+				i = static_cast<int>(c);
+				f = static_cast<float>(c);
+				d = static_cast<double>(c);
+				break;
+			case INT:
+				i = std::stoi(input);
+				c = static_cast<char>(i);
+				f = static_cast<float>(i);
+				d = static_cast<double>(i);
+				break;
+			case FLOAT:
+				f = std::stof(input);
+				i = static_cast<int>(f);
+				c = static_cast<char>(f);
+				d = static_cast<double>(f);
+				break;
+			case DOUBLE:
+				d = std::stod(input);
+				i = static_cast<int>(d);
+				c = static_cast<char>(d);
+				f = static_cast<float>(d);
+				break;
+			default:
+				std::cout << "Could not find type!" << std::endl;
+				break;
+		}
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Not a valid conversion!" << std::endl;
+		return (-1);
+	}
+	return (0);
+}
+
 int	is_int(std::string input)
 {
 	if (input.empty())
@@ -13,29 +56,38 @@ int	is_int(std::string input)
 types get_type(std::string input)
 {
 	if (!isdigit(input[0]) && input.length() == 1)
-		return CHAR;
+		return (CHAR);
 	if (is_int(input))
-		return INT;
+		return (INT);
+	int	dot_pos = input.find('.');
+	if (is_int(input.substr(0, dot_pos - 1)) && is_int(input.substr(dot_pos + 1, input.length())))
+		return (DOUBLE);
+	if (input[input.length()] == 'f' && is_int(input.substr(0, dot_pos - 1))
+		&& is_int(input.substr(dot_pos + 1, input.length() - 1)))
+		return (FLOAT);
+	if (input == "-inff" || input == "+inff" || input == "nanf")
+		return FLOAT;
+	if (input == "-inf" || input == "+inf" || input == "nan")
+		return DOUBLE;
+	return (NONE);
+}
 
-
-	return NONE;
+void	print_con(int i, char c, float f, double d)
+{
+	std::cout << i << std::endl;
+	std::cout << c << std::endl;
+	std::cout << f << std::endl;
+	std::cout << d << std::endl;
 }
 
 void ScalarConverter::convert(std::string input)
 {
+	int	i;
+	char c;
+	float f;
+	double d;
 
-	switch (get_type(input))
-	{
-		case INT:
-
-		case CHAR:
-
-		case FLOAT:
-
-		case DOUBLE:
-
-		default:
-
-	};
-	//static_cast<double>();
+	if (converter(input, i, c, f, d, get_type(input)) == -1)
+		return;
+	print_con(i, c, f, d);
 }
