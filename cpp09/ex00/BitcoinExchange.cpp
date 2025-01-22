f98d08fc21d7a9b33efa7bc264cbb72191348eeb
Aaro Hamalainen <aaro.h4malainen@gmail.com>
2025-01-22 16:04:29 +0200

    Progress on cpp09 ex00

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -12,35 +12,99 @@ BitcoinExchange::BitcoinExchange(std::string filePath)
 
 	// Parsing the database file
 	while (getline(database, line))
-		parseLine(line);
+		parseDatabase(line);
 
 	std::ifstream file(filePath);
 	
 	if (!file.is_open())
 		throw(BitcoinExchange::failedOpen("Input file failed to open"));
 	getline(file, line); // skips info
+
+	// Parsing the input file
+	while (getline(file, line))
+		parseInput(line);
+
 }
 
-void BitcoinExchange::parseLine(std::string line)
+void BitcoinExchange::parseDatabase(std::string line)
 {
 	size_t separator = line.rfind(",");
 
 	std::string date = line.substr(0, separator);
 
 	if (checkDate(date))
-		std::cout << "\n";
+		throw(BitcoinExchange::parsingFail("Database: Format is wrong!"));
+
 
 	std::string value = line.substr(separator + 1, line.size() - separator);
 
-	// error check
-		m_data[date] = value;
-	//std::cout << value << "\n";
+	if (checkFloat(value))
+		throw(BitcoinExchange::parsingFail("Database: Value is not a float!"));
+	m_data[date] = value;
+}
+
+void BitcoinExchange::parseInput(std::string line)
+{
+	size_t separator = line.rfind("|");
+	if (separator == std::string::npos)
+	{
+		std::cerr << "Error: bad input => " << line << "\n";
+		return ;
+	}
+	float value = std::stof(line.substr(separator + 1, std::string::npos));
+
+	if (value > 2147483647.0)
+	{
+		std::cerr << "Error: too large a number\n";
+		return ;
+	}
+	else if (value < 0)
+	{
+		std::cerr << "Error: not a positive number\n";
+		return ;
+	}
+	std::string date = line.substr(0, separator - 1);
+
+	auto it = m_data.lower_bound(date);
+
+	if (it->first != date && it != m_data.begin())
+		it--;
+	float dataValue = std::stof(it->second);
+
+	std::cout << date << " => " << value << " = " << value * dataValue << "\n";
+}
+
+int BitcoinExchange::checkFloat(std::string value)
+{
+	bool dot = false;
+
+	for (char c : value)
+	{
+		if (isdigit(c))
+			continue;
+		else if (c == '.' && dot == false)
+			dot = true;
+		else
+			return (1);
+	}
+	if (value.find(".") == 0 || value.find(".") == value.size() - 1)
+		return (1);
+
+	return (0);
 }
 
 int BitcoinExchange::checkDate(std::string date)
 {
-	// years are 4 long
-	std::cout << date.find("-") << "\n";
+	int i = 0;
+
+	for (char c : date)
+	{
+		if ((i == 4 || i == 6) && c == '-')
+			continue;
+		if (!isdigit(c))
+			return (1);
+		i++;
+	}
 
 	return (0);
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -10,8 +10,10 @@ class BitcoinExchange
 		BitcoinExchange& operator=(const BitcoinExchange&);
 		~BitcoinExchange();
 
-		void parseLine(std::string line);
+		void parseDatabase(std::string line);
+		void parseInput(std::string line);
 		int checkDate(std::string date); // 1 for error and 0 for correct
+		int checkFloat(std::string value); // 1 for error and 0 for correct
 
 		class failedOpen: public std::exception
 		{
@@ -21,6 +23,16 @@ class BitcoinExchange
 			private:
 				std::string m_message;
 		};
+
+		class parsingFail: public std::exception
+		{
+			public:
+				parsingFail(const std::string& message) : m_message(message) {}
+				const char* what() const throw() { return m_message.c_str(); }
+			private:
+				std::string m_message;
+		};
+
 	private:
 		std::map<std::string, std::string> m_data;
 
