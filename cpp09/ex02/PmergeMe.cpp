0dc380b2b07ab9d5ea28c74b6fba3e50496117f4
Aaro Hamalainen <aaro.h4malainen@gmail.com>
2025-02-07 16:19:19 +0200

    Another way to do sorting cpp09 ex03 for consideration

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -4,6 +4,8 @@
 
 void PmergeMe::sortVec()
 {
+	if (m_vector.size() == 1)
+		return ;
 	std::vector<std::pair<int, int>> pairs;
 
 	for (size_t i = 0; i < m_vector.size(); i+= 2)
@@ -16,19 +18,21 @@ void PmergeMe::sortVec()
 		m_vector.push_back(pairs[i].second);
 	}
 
-	for (size_t i = 1; i < m_vector.size(); i++)
-	{
-		int j = i - 1;
-		int num = m_vector[i];
+	sortVec();
 
-		while(j >= 0 && m_vector[j] > num)
-		{
-			m_vector[j + 1] = m_vector[j]; // optimization to make a new vector
-			j--;							// inside the function so it doesnt
-											// ask for m_vector from class constantly
-		}
-		m_vector[j + 1] = num;
-	}
+	//for (size_t i = 1; i < m_vector.size(); i++)
+	//{
+	//	int j = i - 1;
+	//	int num = m_vector[i];
+
+	//	while(j >= 0 && m_vector[j] > num)
+	//	{
+	//		m_vector[j + 1] = m_vector[j]; // optimization to make a new vector
+	//		j--;							// inside the function so it doesnt
+	//										// ask for m_vector from class constantly
+	//	}
+	//	m_vector[j + 1] = num;
+	//}
 	for (size_t i = 0; i < pairs.size(); i++)
 	{
 		std::vector<int>::iterator it = m_vector.begin();
